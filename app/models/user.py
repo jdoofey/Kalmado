3cb7aa03b4dffa0fcc89722f6861a78bb7c438a7
jake matillano <jakematillano.z@gmail.com>
2022-10-25 14:37:46 -0700

    checkpoint for finishing users table

--- app/models/user.py
+++ app/models/user.py
@@ -7,10 +7,14 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    avatar = db.Column(db.String(255))
+    personal_note = db.Column(db.Text, default="Write some notes here!")
 
+    projects = db.relationship("Project")
     @property
     def password(self):
         return self.hashed_password
@@ -23,8 +27,16 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def to_dict(self):
+        projects = {}
+        # TODO check my logic here, projects need to be connected to users
+        for project in self.user_projects:
+            projects[project.id] = {'project_id': project.id, 'project_title':project.title,'project_color':project.color}
+        # thinking ahead about how to return projects connected to users for convenience
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'avatar': self.avatar,
+            'projects': projects
         }
