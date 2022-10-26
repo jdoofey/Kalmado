a36e62749f5c48035dbf71a8dd55a3ce300452c6
jake matillano <jakematillano.z@gmail.com>
2022-10-26 16:13:14 -0700

    working on projects routes and forms

---
+++ app/api/projects_routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from flask_login import login_required, current_user
+from app.models import db, User, Project, Section, Task
+
+projects_routes = Blueprint('projects', __name__)
+
+@projects_routes.route('/', methods=["GET"])
+@login_required
+def get_all_projects():
+  projects = Project.query.order_by(Project.created_at.desc()).all()
+
+  return {project.id: projects.to_dict() for project in projects}
+
+@projects_routes.route('<int:id>', methods=["GET"])
+@login_required
+def get_project_by_id():
+  project = Project.query.get(id)
+
+  return project.to_dict()
+
+@projects_routes.route('/', methods=["POST"])
+@login_required
+def create_project():
+  
